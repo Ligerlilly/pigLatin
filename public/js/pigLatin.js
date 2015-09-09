ea790685e56cc6b29d5fadf765e1c7fa6ca4ecdf
ryan broughan & Jason Lane <student@epicodus.com>
2015-09-09 10:11:34 -0700

    Works for multiple words using module revealing pattern

--- public/js/pigLatin.js
+++ public/js/pigLatin.js
@@ -1,10 +1,26 @@
-var pig = function(word){
-  letters = word.split('');
-  vowels = ['a', 'e', 'i', 'o', 'u'];
-  if (vowels.indexOf(letters[0]) === -1){
-    letters.push(letters.shift());
+var pig = function(){
+  var one, multiple;
+
+  one = function(word) {
+    letters = word.split('');
+    vowels = ['a', 'e', 'i', 'o', 'u'];
+    while (vowels.indexOf(letters[0]) === -1){
+      letters.push(letters.shift());
+    };
+      return letters.join('') + 'ay';
   };
-    return letters.join('') + 'ay';
+
+  multiple = function(phrase) {
+    phrase = phrase.split(" ");
+    for (var i = 0; i < phrase.length; i += 1){
+      // console.log(phrase[i]);
+      phrase[i] = one(phrase[i]);
+    }
+    return phrase.join(" ");
+  };
+  return { one: one,
+           multiple: multiple };
 };
+
 
 module.exports = pig;
--- public/js/tests.js
+++ public/js/tests.js
@@ -1,12 +1,29 @@
 var pig = require('./pigLatin.js');
 var expect = require('chai').expect;
 
-describe('pigLatin', function(){
+
+describe('pig', function(){
+
   it('adds ay to the end if it starts with a vowel', function(){
-    expect(pig("apple")).to.eq("appleay");
+    var word = pig();
+    expect(word.one('apple')).to.eq("appleay");
   });
 
   it('adds first letter and ay to end of word if first letter is not a vowel', function(){
-    expect(pig('play')).to.eq('laypay');
+    var word = pig();
+    expect(word.one('pay')).to.eq('aypay');
+  });
+
+  it('adds all beginning consonants and ay to end of word if first letter is not a vowel', function(){
+    var word = pig();
+    expect(word.one('play')).to.eq('ayplay');
+  });
+});
+
+
+describe('pigWords', function(){
+  it('adds ay to the end of words if they start with a vowel', function(){
+    var phrase = pig();
+    expect(phrase.multiple('apple art')).to.eq('appleay artay');
   });
 });
